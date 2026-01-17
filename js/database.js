42de798d37e8f9c023755dab52a62d109eda73ed
Tim03122009 <tcharvol@gmail.com>
2026-01-18 00:53:42 +0100

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,19 @@
+// Configuration Firebase récupérée de ton compte
+const firebaseConfig = {
+  apiKey: "AIzaSyB1wNN07DtwpymwPIN4v-m-6y3LUCPxKmc",
+  authDomain: "rugby-21a58.firebaseapp.com",
+  databaseURL: "https://rugby-21a58-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "rugby-21a58",
+  storageBucket: "rugby-21a58.appspot.com",
+  messagingSenderId: "535132438806",
+  appId: "1:535132438806:web:a3ef7b1b3ee101c47bd037"
+};
+
+// Connexion au cerveau (Firebase)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, onValue, update };
