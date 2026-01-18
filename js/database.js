174fea0577c36396d233126ba8586c1fde07caef
Tim03122009 <tcharvol@gmail.com>
2026-01-18 01:30:18 +0100

    Update database.js

--- js/database.js
+++ js/database.js
@@ -4,7 +4,8 @@ import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/
 const firebaseConfig = {
   apiKey: "AIzaSyB1wNN07DtwpymwPIN4v-m-6y3LUCPxKmc",
   authDomain: "rugby-21a58.firebaseapp.com",
-  databaseURL: "https://rugby-21a58-default-rtdb.firebaseio.com", 
+  // On utilise l'adresse complète avec la région exacte
+  databaseURL: "https://rugby-21a58-default-rtdb.europe-west1.firebasedatabase.app/", 
   projectId: "rugby-21a58",
   storageBucket: "rugby-21a58.appspot.com",
   messagingSenderId: "535132438806",
