3358dee15d52da418ce1e696119ff1b1c3afe21d
Tim03122009 <tcharvol@gmail.com>
2026-01-18 01:28:50 +0100

    Update database.js

--- js/database.js
+++ js/database.js
@@ -4,8 +4,7 @@ import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/
 const firebaseConfig = {
   apiKey: "AIzaSyB1wNN07DtwpymwPIN4v-m-6y3LUCPxKmc",
   authDomain: "rugby-21a58.firebaseapp.com",
-  // J'ai mis à jour le lien ci-dessous selon ton erreur console :
-  databaseURL: "https://rugby-21a58-default-rtdb.europe-west1.firebasedatabase.app/", 
+  databaseURL: "https://rugby-21a58-default-rtdb.firebaseio.com", 
   projectId: "rugby-21a58",
   storageBucket: "rugby-21a58.appspot.com",
   messagingSenderId: "535132438806",
